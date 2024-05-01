6c4eed87d07ee7933f11022c315f8477fbbf0351
appcessorize <rudecaptcha@outlook.com>
2024-05-01 22:57:44 +0100

    animations fix music pr friend list started

---
+++ screens/AddFriends.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AddFriends = () => {
+  return (
+    <View>
+      <Text>add friends</Text>
+    </View>
+  );
+};
+
+export default AddFriends;
--- screens/NewsReport.js
+++ screens/NewsReport.js
@@ -12,10 +12,11 @@ import { Audio, Video } from "expo-av";
 
 import * as Speech from "expo-speech";
 import { Ionicons } from "@expo/vector-icons";
+
 const videos = {
-  newsintro: require("../assets/videos/newsintro.mp4"),
+  newsintro: require("../assets/videos/MENNintroLow.mp4"),
   hugoSleeping: require("../assets/videos/hugoSleeping.mp4"),
-  mennIntro: require("../assets/videos/mennIntro.mov"),
+  mennIntro: require("../assets/videos/MENNintroWithMusic.mp4"),
   test: require("../assets/videos/test.mov"),
   lindaCoffee: require("../assets/videos/lindaCoffee.mp4"),
   goalScorer: require("../assets/videos/goalScorer.mp4"),
@@ -26,22 +27,22 @@ const videos = {
 const NewsReport = () => {
   //dummy Ai generated content for the demo
   const anchorScript = [
-    {
-      videoKey: "mennIntro",
-      bannerText: "Rainy weather tomorrow, with mild temperatures.",
-      speech:
-        "Moderate rain is expected tomorrow. The minimum temperature will be 8.9 degrees Celsius, with a maximum of 11 degrees Celsius.",
-      squareBg: "Rainy",
-      squareBgEmoji: "🌧️",
-    },
-    {
-      speech:
-        "Welcome to me news network news. a.i genrated personalized news made with your friends and family",
-      videoKey: "mennIntro",
-      squareBg: "NEWS",
-      squareBgEmoji: "📰",
-      bannerText: "Welcome to the news",
-    },
+    // {
+    //   videoKey: "mennIntro",
+    //   bannerText: "Rainy weather tomorrow, with mild temperatures.",
+    //   speech:
+    //     "Moderate rain is expected tomorrow. The minimum temperature will be 8.9 degrees Celsius, with a maximum of 11 degrees Celsius.",
+    //   squareBg: "Rainy",
+    //   squareBgEmoji: "🌧️",
+    // },
+    // {
+    //   speech:
+    //     "Welcome to me news network news. a.i genrated personalized news made with your friends and family",
+    //   videoKey: "mennIntro",
+    //   squareBg: "NEWS",
+    //   squareBgEmoji: "📰",
+    //   bannerText: "Welcome to the news",
+    // },
     {
       speech: "Paul is practicing for his big performance on the weekend",
 
@@ -91,13 +92,19 @@ const NewsReport = () => {
       bannerText: "See you at 8pm tomorrow",
     },
   ];
+
   const animation = useRef(null);
+  const [introPassed, setIntroPassed] = useState(false);
   const indexRef = useRef(0);
+  const volumeRef = useRef(0);
   const videoRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [playVideo, setPlayVideo] = useState(false);
   const [updateStarted, setUpdateStarted] = useState(false);
-
+  const [hideStartScreen, setHideStartScreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+  const [sound, setSound] = useState(null);
+  const [musicPlaying, setMusicPlaying] = useState(false);
   useEffect(() => {
     if (isPlaying) {
       animation.current.play();
@@ -107,15 +114,24 @@ const NewsReport = () => {
   }, [isPlaying]);
 
   // native tts
-  const [i, setI] = useState(0);
+
   const onTtsdone = () => {
     // animation.current.reset();
     // videoRef.current.playAsync();
     animation.current.reset();
+
+    console.log("volume", volume);
     setPlayVideo(true);
   };
 
   const speak = () => {
+    console.log("speak");
+    setVolume(0.1);
+    volumeRef.current = 0.1;
+    if (!musicPlaying) {
+      playMusic();
+    }
+
     const thingToSay = anchorScript[indexRef.current].speech;
     Speech.speak(thingToSay, { onDone: onTtsdone });
     let isSpeaking = Speech.isSpeakingAsync();
@@ -127,22 +143,93 @@ const NewsReport = () => {
   //video
   useEffect(() => {
     if (playVideo && videoRef.current) {
+      setVolume(0);
+      volumeRef.current = 0;
+
       videoRef.current.playAsync();
     }
   }, [playVideo]);
 
   const handleVideoStatusUpdate = async (status) => {
     if (status.didJustFinish && !status.isLooping) {
-      // The video has finished playing and it is not looping
-
+      if (!updateStarted) {
+        //speech and then
+        setUpdateStarted(true);
+        setIntroPassed(true);
+      }
       await videoRef.current.setPositionAsync(0); // Reset video to start
       setPlayVideo(false); // Optionally stop the video from auto-restarting
       //   setI((prevI) => prevI + 1);
-      indexRef.current = indexRef.current + 1;
-      console.log(" vid status i", i);
+      if (updateStarted) {
+        indexRef.current = indexRef.current + 1;
+      }
+
       speak();
     }
   };
+  const startNewsReport = () => {
+    if (introPassed) {
+      setUpdateStarted(true);
+      speak();
+    } else {
+      setPlayVideo(true);
+
+      setHideStartScreen(true);
+    }
+  };
+
+  let videoSource;
+  if (introPassed) {
+    videoSource = videos[anchorScript[indexRef.current].videoKey];
+  } else {
+    videoSource = require("../assets/videos/MENNintroWithMusic.mp4");
+  }
+  async function playMusic() {
+    console.log("Playing Sound..");
+    // Make sure the MP3 file is correctly imported and the path is correct
+    const audioUri = require("../assets/music/bgmusic.mp3");
+    try {
+      // Preloading the audio file and then playing it
+      const { sound: newSound } = await Audio.Sound.createAsync(
+        audioUri, // directly use the asset reference here
+        { shouldPlay: true, isLooping: true } // Optionally preload and start playing immediately
+      );
+      setSound(newSound);
+      await sound.playAsync();
+      setMusicPlaying(true); // This should already be playing if 'shouldPlay' is true
+    } catch (error) {
+      console.log("Error loading or playing audio:", error);
+    }
+  }
+  useEffect(() => {
+    console.log("useeffectvolume", volume);
+    if (sound) {
+      if (playVideo) {
+        sound.setVolumeAsync(0).catch((error) => {
+          console.log("Error setting volume:", error);
+        });
+      } else {
+        sound.setVolumeAsync(0.1).catch((error) => {
+          console.log("Error setting volume:", error);
+        });
+      }
+      //   sound.setVolumeAsync(volumeRef.current).catch((error) => {
+      //     console.log("Error setting volume:", error);
+      //   });
+    }
+  }, [volume, sound, volumeRef.current]);
+
+  useEffect(() => {
+    console.log(
+      "indexref",
+      indexRef.current,
+      anchorScript[indexRef.current].speech,
+      anchorScript[indexRef.current].bannerText,
+      anchorScript[indexRef.current].videoKey,
+      anchorScript[indexRef.current].squareBg,
+      anchorScript[indexRef.current].squareBgEmoji
+    );
+  }, [indexRef.current]);
 
   return (
     <View className="w-full h-full bg-blue-50 flex items-center justify-end">
@@ -150,7 +237,15 @@ const NewsReport = () => {
         source={require("../assets/newsBgEdited.jpeg")}
         className="w-full h-full absolute top-0 left-0"
       />
-
+      {!introPassed && (
+        <View className="absolute z-10 top-[50] right-0 pointer-events-none flex-row  bg-red-600 shadow">
+          <View className="bg-red-600 px-1 py-1 items-center justify-center flex flex-row">
+            <Text className="text-3xl text-white shadow font-extrabold italic pr-1">
+              NIGHTLY NEWS
+            </Text>
+          </View>
+        </View>
+      )}
       {!playVideo && (
         <View
           className={`absolute top-20 right-10 rounded bg-red-500 w-[150] h-[150] flex items-center justify-center shadow-md`}
@@ -169,13 +264,12 @@ const NewsReport = () => {
         </View>
       )}
 
-      {!updateStarted && (
+      {!hideStartScreen && (
         <View className="bg-white w-full h-full flex justify-center items-center z-50 absolute top-30">
           <Button
             title="Start"
             onPress={() => {
-              setUpdateStarted(true);
-              speak();
+              startNewsReport();
             }}
           />
         </View>
@@ -187,13 +281,6 @@ const NewsReport = () => {
           style={styles.lottieStyles}
           source={require("../assets/robotLottieBodyOpti.json")}
         />
-
-        // <LottieView
-        //   // autoPlay
-        //   ref={animation}
-        //   style={styles.lottieStyles}
-        //   source={require("./assets/mouthO.json")}
-        // />
       )}
 
       {!playVideo && (
@@ -206,13 +293,10 @@ const NewsReport = () => {
       )}
       <Video
         ref={videoRef}
-        // style={styles.video}
-        source={videos[anchorScript[indexRef.current].videoKey]}
-        // source={require("./assets/lindaCoffee.mp4")}
-        //   uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        source={videoSource}
+        // source={{  //   uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
         // }} // Update with your video URL
         resizeMode="cover"
-        // resizeMode={ResizeMode.CONTAIN}
         useNativeControls={false}
         onPlaybackStatusUpdate={(status) => {
           handleVideoStatusUpdate(status);
@@ -230,16 +314,24 @@ const NewsReport = () => {
         showPoster={true}
         isLooping={false}
         className={` absolute ${playVideo ? "w-full h-full top-0" : "hidden"}`}
-        // className={` absolute ${playVideo ? "w-full h-full top-0" : "hidden"}`}
       />
 
-      {playVideo && (
-        // <View className="w-screen bg-red-600 py-2 border-y-1 border-white items-start pl-8 justify-left absolute z-10 bottom-20 shadow-md">
-        //   <Text className="text-yellow-200 text-xl font-semibold">
-
-        //   </Text>
-        // </View>
-
+      {!introPassed && (
+        <View className="absolute z-10 bottom-20  left-0 pointer-events-none flex-row w-full bg-white shadow">
+          <View className="bg-red-600 px-1 py-1 items-center justify-center flex flex-row">
+            <Ionicons name="globe-outline" size={22} color="white" />
+            <Text className=" text-xl text-white shadow font-extrabold italic pr-1">
+              MENN
+            </Text>
+          </View>
+          <View className="bg-white px-1 py-1 items-center justify-center ">
+            <Text className="pl-4  text-gray-700 shadow  from-semibold text-xl">
+              Welcome to the news
+            </Text>
+          </View>
+        </View>
+      )}
+      {playVideo && introPassed && (
         <View className="absolute z-10 bottom-20  left-0 pointer-events-none flex-row w-full bg-white shadow">
           <View className="bg-red-600 px-1 py-1 items-center justify-center flex flex-row">
             <Ionicons name="globe-outline" size={22} color="white" />
